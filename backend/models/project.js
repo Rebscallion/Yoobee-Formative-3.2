844dfed7cb895499872a31743f8ff48bb67d2ffa
Rebscallion <rebekah.foley.read@gmail.com>
2022-08-22 19:40:31 +1200

    Started MongoDB, connected to project, pushed DB to frontend

---
+++ backend/models/project.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const projectSchema = new mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        image_url: String,
+        name: String,
+        project: String
+    },
+    {
+        versionKey: false
+    }
+);
+
+module.exports = mongoose.model('Projects', projectSchema);
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+console.log("hello");
+
+let showAllProjects = () => {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/allProjects",
+    success: (projects) => {
+      console.log(projects);
+      renderProjects(projects);
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+let renderProjects = (projects) => {
+    console.log("The render project function is running");
+    result.innerHTML = "";
+    projects.forEach((item) => {
+      result.innerHTML += `
+        <div class="result-container">
+        <img src="${item.image_url}" alt="${item.name}">
+        <h3>${item.name}</h3>
+        <p>${item.project}</p> 
+        </div>
+        `;
+    });
+  };
+  
+  //-----start app-----
+  showAllProjects();
